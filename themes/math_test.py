1ada8d2ccf8bca08cc5cb602f51084079014f533
Albert <albert.koch1973@gmail.com>
2020-05-05 14:48:31 +0500

    removed trash

--- themes/math_test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-with open('soc', 'r') as file:
-    params = json.loads(file.read())
-r = requests.post('https://soc-ege.sdamgia.ru/test?a=generate',params = params)
-print(r.url)
